ab47fe703c833ea695d53878b6677d3e1b54ef97
renan <refreitas.ie@gmail.com>
2023-11-13 17:08:56 +0000

    Configurado parametrização para testes

---
+++ libpythonpro/tests/test_spam/test_enviador_de_email.py
@@ -0,0 +1,24 @@
+import pytest
+
+from libpythonpro.spam.enviador_de_email import Enviador
+
+
+def test_criar_enviador_de_email():
+    enviador = Enviador()
+    assert enviador is not None
+
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['renanfreeiitas@gmail.com', 'refreitas.ie@gmail.com']
+)
+def test_remetente(remetente):
+    enviador = Enviador()
+
+    resultado = enviador.enviar(
+        remetente,
+        'renanfreeiitas@gmail.com',
+        'Cursos Python Pro',
+        'Primeiro turma Guido Von Rossum aberta'
+    )
+    assert remetente in resultado
