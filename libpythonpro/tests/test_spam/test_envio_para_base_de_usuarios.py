cd001cea379ad2ec0d0d3e649bca948d5d0df977
renan <refreitas.ie@gmail.com>
2023-11-15 20:40:57 +0000

    configurados usuarios para envio de email utilizando o decorador parametrize.

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,11 +1,30 @@
+import pytest
+
 from libpythonpro.spam.enviador_de_email import Enviador
 from libpythonpro.spam.main import EnviadorDeSpam
+from libpythonpro.spam.modelos import Usuario
 
 
-def test_envio_de_spam(sessao):
-    enviador_de_spam = EnviadorDeSpam(sessao, Enviador)
+@pytest.mark.parametrize(
+    'usuarios',
+    [
+        [
+            Usuario(nome='Renan', email='refreitas.ie@gmail.com'),
+            Usuario(nome='Agatha', email='refreitas.ie@gmail.com')
+        ],
+        [
+            Usuario(nome='Renan', email='refreitas.ie@gmail.com')
+        ]
+    ]
+)
+def test_qtd_de_spam(sessao, usuarios):
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    enviador = Enviador()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'renanfreeiitas@gmail.com',
         'Cursos Python Pro',
         'Confira os modulos fantásticos do Python!'
     )
+    assert len(usuarios) == enviador.qtd_email_enviados
--- libpythonpro/tests/test_spam/test_usuario.py
+++ libpythonpro/tests/test_spam/test_usuario.py
@@ -2,13 +2,16 @@ from libpythonpro.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='Renan')
+    usuario = Usuario(nome='Renan', email='refreitas.ie@gmail.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuario(sessao):
-    usuarios = [Usuario(nome='Renan'), Usuario(nome='Agatha')]
+    usuarios = [
+        Usuario(nome='Renan', email='refreitas.ie@gmail.com'),
+        Usuario(nome='Agatha', email='refreitas.ie@gmail.com')
+    ]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
