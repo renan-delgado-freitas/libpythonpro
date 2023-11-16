87a15d0add6cd41e4509b5b399d77f5e26c4849e
renan <refreitas.ie@gmail.com>
2023-11-16 15:34:34 +0000

    configurado teste do modulo github_api, com o devido isolamento de teste.

--- libpythonpro/spam/enviador_de_email.py
+++ libpythonpro/spam/enviador_de_email.py
@@ -1,5 +1,5 @@
 class Enviador:
-    def enviar(self, remetente, destinatario=None, assunto='', corpo=''):
+    def enviar(self, remetente, destinatario, assunto, corpo):
         if '@' not in remetente:
             raise EmailInvalido(f'Email de remetente invalido: {remetente}')
         return remetente
