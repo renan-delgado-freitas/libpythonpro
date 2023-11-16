2af7c12d52dfe885b113d9d7ac7561d33c423f12
renan <refreitas.ie@gmail.com>
2023-11-16 15:20:58 +0000

    configurado mock para isolamento de test unitario

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,3 +1,5 @@
+from unittest.mock import Mock
+
 import pytest
 
 from libpythonpro.spam.enviador_de_email import Enviador
@@ -20,11 +22,29 @@ from libpythonpro.spam.modelos import Usuario
 def test_qtd_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = Enviador()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'renanfreeiitas@gmail.com',
         'Cursos Python Pro',
         'Confira os modulos fantásticos do Python!'
     )
-    assert len(usuarios) == enviador.qtd_email_enviados
+    assert len(usuarios) == enviador.enviar.call_count
+
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome='Renan', email='refreitas.ie@gmail.com')
+    sessao.salvar(usuario)
+    enviador = Mock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        'agatha@gmail.com',
+        'Cursos Python Pro',
+        'Confira os modulos fantásticos do Python!'
+    )
+    enviador.enviar.assert_called_once_with(
+        'agatha@gmail.com',
+        'refreitas.ie@gmail.com',
+        'Cursos Python Pro',
+        'Confira os modulos fantásticos do Python!'
+    )
