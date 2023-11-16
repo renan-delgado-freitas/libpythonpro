32631611898b253e5736bf5e8e5fc6a82f81eb4d
renan <refreitas.ie@gmail.com>
2023-11-16 16:31:56 +0000

    finalizado modulo de injeção de dependencias com utilização da lib (pytest-mock)

--- libpythonpro/tests/test_spam/test_github_api.py
+++ libpythonpro/tests/test_spam/test_github_api.py
@@ -1,14 +1,28 @@
 from unittest.mock import Mock
 
+import pytest
+
 from libpythonpro import github_api
 
 
-def test_buscar_avatar():
+@pytest.fixture
+def avatar_url(mocker):
     resp_mock = Mock()
+    url = 'https://avatars.githubusercontent.com/u/135972069?v=4'
     resp_mock.json.return_value = {
-        'login': 'renan-delgado-freitas', 'id': 135972069, 'node_id': 'U_kgDOCBrE5Q',
-        'avatar_url': 'https://avatars.githubusercontent.com/u/135972069?v=4', 'gravatar_id': '',
+        'login': 'renan-delgado-freitas', 'id': 135972069,
+        'avatar_url': url
     }
-    github_api.requests.get = Mock(return_value=resp_mock)
+    get_mock = mocker.patch('libpythonpro.github_api.requests.get')
+    get_mock.return_value = resp_mock
+    return url
+
+
+def test_buscar_avatar(avatar_url):
     url = github_api.buscar_avatar('renan-delgado-freitas')
-    assert 'https://avatars.githubusercontent.com/u/135972069?v=4' == url
+    assert avatar_url == url
+
+
+def test_buscar_avatar_integracao():
+    url = github_api.buscar_avatar('renanfreeiitas')
+    assert 'https://avatars.githubusercontent.com/u/38738883?v=4' == url
